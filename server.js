7e6b9d089c25e062e4a5f613b62217564641ccea
Zida <shawzida@gmail.com>
2014-10-31 17:18:53 +0800

    修改样式，添加多说评论，站长统计(功能还需测试)

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
     res.end('<p>Hello World</p>');
-}).listen();
+}).listen(port);
